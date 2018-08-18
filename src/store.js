6620da7273e8d153a138fcd91299eb4ab6c36b6e
Johknotter Hill <johknotterhill@gmail.com>
2018-08-18 15:03:41 -0400

    reinitializing

--- src/store.js
+++ src/store.js
@@ -1,13 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
-import { composeWithDevTools } from 'redux-devtools-extension';
+
 
 const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middleware),
+const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
 
